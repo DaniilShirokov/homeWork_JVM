2e8e77caed498c55feabb0f537d373fbde4ad597
Damiil Shirokov <daniil196196@gmail.com>
2024-06-12 23:04:04 +0500

    update

--- src/main/java/ru/netology/JvmComprehension.java
+++ src/main/java/ru/netology/JvmComprehension.java
@@ -1,2 +1,18 @@
-package ru.netology;public class JvmComprehension {
+package ru.netology;
+
+public class JvmComprehension { //резервируем область памяти в metaspace
+
+        public static void main(String[] args) { //создаем фрейм main в стеке
+            int i = 1;                      //  добавляем в фрейм main
+            Object o = new Object();        // добавляе объекта в кучу, и создание ссылки на объект во фрейме main, так же работает загрузчик к Bootstrap ClassLoader
+            Integer ii = 2;                 // добавляем объект добавляем в фрейм main и в кучу
+            printAll(o, i, ii);             // создается фрейм printAll, работает загрузчик классов Platform ClassLoader, связываем два фрейма через ссылки в куче
+            System.out.println("finished"); // используется фрейм который был создан раньше, туда передается значение которое будет записано в куче
+        }
+
+        private static void printAll(Object o, int i, Integer ii) {
+            Integer uselessVar = 700;                   //  добавляем во фрейм printAll
+            System.out.println(o.toString() + i + ii);  // создаем еще один фрейм, куда передаем из кучи данные
+        }
 }
+
